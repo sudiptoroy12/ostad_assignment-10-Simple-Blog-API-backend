0245e7bc4b7dae17fa94dc4ba437d71e12659617
sudiptoroy12 <sudipto1294@gmail.com>
2026-08-19 21:29:14 +0600

    Update MongoDB connection string to include database name

--- src/db/db.js
+++ src/db/db.js
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(process.env.MONGODB_URL)
-    
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${process.env.DB_NAME}`,
+    );
     console.log(
       `MongoDB connected !!! DB HOST : ${connectionInstance.connection.host}`,
     );
